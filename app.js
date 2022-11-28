8984659bc611930f8886f2ee3b6bb1ccd75ad10b
satvikkhetan7 <satvikkhetancode@gmail.com>
2022-11-28 17:47:36 -0500

    updating connection string

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
+const CONNECTION_STRING = 'mongodb+srv://satvikkhetan:WebDevAssignment2022@cluster0.qszkev9.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(CONNECTION_STRING);
 HelloController(app)
 UsersController(app)
